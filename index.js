845c9ed07b159e5874395f1ba411101ae78d3a3c
Michael Blandford <mdblandford@gmail.com>
2018-03-26 19:25:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ function destructivelyAppendKitten(Ralph){kittens.push(Ralph);return kittens}
 function destructivelyPrependKitten(Bob){kittens.unshift(Bob);return kittens}
 function destructivelyRemoveLastKitten(Ralph){kittens.pop(Ralph);return kittens}
 function destructivelyRemoveFirstKitten(Bob){kittens.shift(Bob);return kittens}
-function appendKitten
+function appendKitten(Broom){return[...kittens,Broom]}
