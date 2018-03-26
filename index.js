ea9e01a192abff8995b81af73d64b6682675e894
Michael Blandford <mdblandford@gmail.com>
2018-03-26 19:18:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 var kittens = ["Milo", "Otis", "Garfield"]
 function destructivelyAppendKitten(Ralph){kittens.push(Ralph);return kittens}
-function destructivelyPrependKitten(Bob){
+function destructivelyPrependKitten(Bob){kittens.unshift(Bob);return kittens}
