dfd4849f569d46d6e42685118f9c18467da0a438
Michael Blandford <mdblandford@gmail.com>
2018-03-26 19:29:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,4 +5,4 @@ function destructivelyRemoveLastKitten(Ralph){kittens.pop(Ralph);return kittens}
 function destructivelyRemoveFirstKitten(Bob){kittens.shift(Bob);return kittens}
 function appendKitten(Broom){return[...kittens,Broom]}
 function prependKitten(Arnold){return[Arnold,...kittens]}
-function removeLastKitten(Garfield){return[]
+function removeLastKitten(Garfield){return kittens.slice(0,kittens.length-1)}
